09fef2fdac1f56be37363115ce08490eaf2f5fe9
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-04-30 21:46:29 +0300

    infinite scroll and litebox

--- src/index.js
+++ src/index.js
@@ -7,67 +7,46 @@ import 'material-design-icons/iconfont/material-icons.css';
 PNotify.defaults.styling = 'material';
 PNotify.defaults.icons = 'material';
 
-import { imageList, searchForm, btnSearch } from './js/refs';
-import fetchItem from './js/fetchImg';
+import { imageList, searchForm } from './js/refs';
 import imgItem from './tmpl/imgItem.hbs';
 import './sass/styles.scss';
 
-//const apiKey = '16137003-a99878a83e3cf9a5973a72148';
+const apiKey = '16137003-a99878a83e3cf9a5973a72148';
+const cors_api_host = 'https://cors-anywhere.herokuapp.com/';
+let searchQuery;
 
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
 
     const form = event.currentTarget;
-    fetchItem.query = form.elements.query.value;
+    searchQuery = form.elements.query.value;
+    console.log(searchQuery);
 
-    fetchItem.reset();
     imageList.innerHTML = '';
-    //infScroll.pageIndex = 1;
-    btnSearch.classList.add('hidden');
-    if (!fetchItem.query) {
+    infScroll.pageIndex = 1;
+
+    if (!searchQuery) {
         PNotify.error({ title: 'Oooopsy', text: 'input mistake' });
         return;
     }
-
-    nextPageMarkup();
+    infScroll.loadNextPage();
 });
 
-btnSearch.addEventListener('click', () => {
-    nextPageMarkup();
-});
-
-/*window.addEventListener('scroll', () => {
-    infScroll().then(nextPageMarkup());
-});*/
-
-/*imageList.addEventListener('click', event => {
+imageList.addEventListener('click', event => {
     if (event.target.nodeName !== 'IMG') return;
     if (event.target.dataset.imgbig != null) {
-        basicLightbox.create(`<img src="${event.target.dataset.imgbig}">`).show();
+        basicLightbox
+            .create(`<img src="${event.target.dataset.imgbig}">`)
+            .show();
     }
-});*/
+});
 
-function imgMarkup(hits) {
-    const markup = imgItem(hits);
-    imageList.insertAdjacentHTML('beforeend', markup);
-}
-
-function nextPageMarkup() {
-    fetchItem.fetch().then(hits => {
-        imgMarkup(hits);
-        btnSearch.classList.remove('hidden');
-        window.scrollTo({
-            top: imageList.scrollHeight,
-            behavior: 'smooth',
-        });
-    });
-}
-
-/*const infScroll = new infiniteScroll(imageList, {
+const infScroll = new infiniteScroll('.gallery', {
     path: function () {
         return (
+            cors_api_host +
             'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=' +
-            fetchItem.query +
+            searchQuery +
             '&page=' +
             this.pageIndex +
             '&per_page=12&key=' +
@@ -76,4 +55,15 @@ function nextPageMarkup() {
     },
     history: false,
     responseType: 'text',
-});*/
+    scrollThreshold: 100,
+});
+
+//const proxyFix = new Proxy();
+
+infScroll.on('load', response => {
+    const dataInfo = JSON.parse(response);
+    const cards = dataInfo.hits;
+
+    const markup = imgItem(cards);
+    imageList.insertAdjacentHTML('beforeend', markup);
+});
